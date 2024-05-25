ea22f559cba80c1ab22644872735739196059dd1
iTzChrisDev <chrissarmiento200203@gmail.com>
2024-05-24 20:42:32 -0600

    UPDATE CSS

--- Practica7 - Portfolio/script.js
+++ Practica7 - Portfolio/script.js
@@ -2,36 +2,39 @@ const nameSpan = document.getElementById("name");
 let index = 0;
 
 const frases = [
-  "Chris",
-  "Developer",
-  "Guapo",
-  "Poderoso",
-  "Hermoso",
-  "Precioso",
-  "Armonioso",
-  "Un buen socio",
-  "Misterioso",
-  "Buena gente",
-  "Detergente",
-  "Muy majete",
-  "Inteligente",
-  "Nada ojete",
-  "Irreverente",
-  "Un exponente",
+  "Chris.",
+  "Developer.",
+  "Ingenioso.",
+  "Programador."
 ];
 
-function getFrases() {
-  return frases[index];
+function typePhrase(phrase, callback) {
+  let i = 0;
+  nameSpan.innerHTML = ""; 
+  const interval = setInterval(() => {
+    if (i < phrase.length) {
+      nameSpan.innerHTML += phrase.charAt(i);
+      i++;
+    } else {
+      clearInterval(interval);
+      if (callback) callback();
+    }
+  }, 100); 
+}
+
+function startTyping() {
+  typePhrase(frases[index], function () {
+    setTimeout(function () {
+      if (index < frases.length - 1) {
+        index++;
+      } else {
+        index = 0;
+      }
+      startTyping();
+    }, 1500); 
+  });
 }
 
 onload = function () {
-  nameSpan.innerHTML = getFrases();
-  setInterval(function () {
-    nameSpan.innerHTML = getFrases();
-    if (index < frases.length - 1) {
-      index++;
-    } else {
-      index = 0;
-    }
-  }, 2000);
+  startTyping();
 };
